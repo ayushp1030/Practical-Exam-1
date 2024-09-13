d3df33ecdd58a048f84627877ab13d9deccfeb9a
AYUSH PATEL <ayushp0848@gmail.com>
2024-09-13 18:39:35 +0530

    Add files via upload

---
+++ ayush akhil sir exam 1/Untitled1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    
+    for (char letter = 'a'; letter <= 'z'; letter++) 
+	{
+        
+        printf("%c ", letter);
+    }
+    printf("\n"); 
+
+    return 0;
+}
+
---
+++ ayush akhil sir exam 1/Untitled3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main() 
+{
+    int num ;
+    
+    printf(" JAY SHREE RAM  : ");
+    scanf("%d num");
+    printf(" JAY SHREE RAM  : ");
+    printf(" JAY SHREE RAM  : ");
+    printf(" JAY SHREE RAM  : ");
+    
+}
+
